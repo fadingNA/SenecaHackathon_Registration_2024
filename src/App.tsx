d552a3b3cb104e03701f1df45a5c55acc4794745
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-01-21 08:44:25 -0500

    remove getparti

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import './App.css';
+import "./App.css";
 
-import RegistrationForm from './components/RegistrationForm';
-import Navbar from './components/NavBar';
-import Footer from './components/Footer';
-import GetAllParticipant from './components/participants/GetAllParticipant';
-import Confirmation from './components/Confirmation';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import React from 'react';
+import RegistrationForm from "./components/RegistrationForm";
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Confirmation from "./components/Confirmation";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from "react";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/success/:id',
+      path: "/success/:id",
       element: <Confirmation></Confirmation>,
     },
     {
-      path: '/',
+      path: "/",
       element: <RegistrationForm></RegistrationForm>,
-    },
-    {
-      path: '/gapfb',
-      element: <GetAllParticipant></GetAllParticipant>,
     },
   ]);
   return (

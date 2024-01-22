75650c650513ddab20e3cdcdbf54057e94addb1c
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-01-22 00:44:56 -0500

    fix firebase variables

--- src/model/data/firebase/Firebase_config.ts
+++ src/model/data/firebase/Firebase_config.ts
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig: FirebaseConfig = {
-  apiKey: import.meta.env.VITE_API_FB_KEY || "",
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN || "",
-  projectId: import.meta.env.VITE_PROJECT_ID || "",
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || "",
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || "",
-  appId: import.meta.env.VITE_APP_ID || "",
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID || "",
+  apiKey: import.meta.env.VITE_API_FB_KEY || 'AIzaSyDl4-QqV8EU5PO34jMGrT0Q1u4t6QN7Dsw',
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN || 'senecahackathonregistration.firebaseapp.com',
+  projectId: import.meta.env.VITE_PROJECT_ID || 'senecahackathonregistration',
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || 'senecahackathonregistration.appspot.com',
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || '17328393836',
+  appId: import.meta.env.VITE_APP_ID || '1:17328393836:web:2e4dba9eea78cf90221249',
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID || 'G-5QWBY15E71',
 };
 
 const app = initializeApp(firebaseConfig);
